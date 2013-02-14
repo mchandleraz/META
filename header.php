b19252254e761ceb93b7271190587871b4b3a83f
mchandleraz <matt@mchandler.me>
2013-02-14 09:39:20 -0700

    Removed db info

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 session_start();
 
 $dbhost = "localhost";
-$dbname = "chopcult";
-$dbuser = "php";
-$dbpass = "password";
+$dbname = "";
+$dbuser = "";
+$dbpass = "";
 
 mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
